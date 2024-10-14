9243a8975ddfa5c352d42c18afe521a497fb2986
Shyphem <reyevan21@gmail.com>
2024-10-14 12:26:21 +0200

    index.js modif - crea annonce js

---
+++ le-coin-dls/pages/components/annonce.js
@@ -0,0 +1,10 @@
+export default function Annonce() {
+    const annonceTitre = "TEST 1 2 1 3";
+    const prix = "";
+
+    return (
+        <>
+            <h1>{annonceTitre}</h1>
+        </>
+    )
+}
